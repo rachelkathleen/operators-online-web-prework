c9dc3c35481e9ef5d6417b8b67b33266708e5f8f
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-03 20:25:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	else
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+# conditional ? action_if_true : action_if_false
+speed > 60 || speed < 40 ? true : false
 end
-	
-
-
