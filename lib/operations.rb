df614c5883541e7a85d5a258be654845af58c9e5
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-03 20:21:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,15 @@
 def unsafe?(speed)
 	if speed > 60
 		return true
-	elsif speed < 40 
+	elsif speed < 40
 		return true
-	else 
+	else
 		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
